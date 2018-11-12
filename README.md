92a0fdb27edb6b0f555c0e418fc49bc8e9a4cb06
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-12 23:37:54 +0100

    add test cases

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-
+1. GET /rd/endpoints
+2. automatically create custom API e.g. /{resource.if] + 's'/{resource.rt} + 's'/:id
+3. PUT /mapping
+4. fetch all resources: sensors should be read-only, actors read and write; positive and negative tests covering all error statuses (a PUT to a sensor should return "METHOD NOT ALLOWED" status etc.)
+5. stop serving resources
+6. try to fetch stopped resources -> gateway should return "BAD GATEWAY" status or similiar
+7. start serving resources again
+8. fetch resources
+9. change return type of resources to non-json or whatever
+10. fetch resources -> verify appropriate error status
+11. PUT /mapping with empty mapping
+12. all resources should return 404 now
--- test/test.js
+++ test/test.js
@@ -12,11 +12,12 @@ console.log(`
 `);
 
 // --- config ---
-const log = false; // set true to print test data
+const logEnabled = false; // set true to print test data
 
 // --- global test variables ---
 let endpoints = [];
 let resources = [];
+let mapping = {};
 
 // --- test cases ---
 describe('GET /rd/endpoints', () => {
@@ -27,34 +28,104 @@ describe('GET /rd/endpoints', () => {
     endpoints = res.body;
     expect(endpoints).to.be.an('array');
     expect(endpoints.length).to.be.greaterThan(0);
-    if (log) console.log('returns:');
-    if (log) console.log(endpoints);
+    log('returns:');
+    log(endpoints);
   });
 });
 
 describe('The returned endpoints array', () => {
-  it('should consist of endpoints containing resources', async () => {
+  it('should consist of endpoints containing resources', () => {
     expect()
     resources = extractResources(endpoints);
     expect(resources).to.be.an('array');
     expect(resources.length).to.be.greaterThan(0);
-    if (log) console.log('resources extracted from endpoints:');
-    if (log) console.log(resources);
+    log('resources extracted from endpoints:');
+    log(resources);
+  });
+});
+
+describe('The extracted resources', () => {
+  it('should contain "base", "if", "path" and "rt" properties', () => {
+    resources.forEach((resource, index) => {
+      expect(resource.base).to.be.a('string');
+      expect(resource.base.length).to.be.greaterThan(0);
+      expect(resource.path).to.be.a('string');
+      expect(resource.path.length).to.be.greaterThan(0);
+      expect(resource.rt).to.be.a('string');
+      expect(resource.rt.length).to.be.greaterThan(0);
+      mapping[`/${resource.if}s/${resource.rt}/${index}`] = (
+        resource.base + resource.path
+      );
+    })
+    log('Mapping generated from resources:')
+    log(mapping);
+  });
+});
+
+describe('PUT /mapping', () => {
+  it('should return status 204 (NO CONTENT)', async () => {
+    const res = await chai.request(url).put('/mapping').send(mapping);
+    expect(res.status).to.equal(204);
   });
 });
 
 describe('GET /mapping', () => {
-  it('should return a JSON object', async () => {
+  it('should return the same mapping', async () => {
     const res = await chai.request(url).get('/mapping');
     expect(res.status).to.equal(200);
     expect(res).to.be.json;
     expect(res.body).to.be.an('object');
+    expect(res.body).to.deep.equal(mapping);
+  });
+});
+
+describe('GET and PUT /gateway', () => {
+  it('should forward requests to resources based on the mapping', async () => {
+    Object.keys(mapping).forEach(async path => {
+      const res1 = await chai.request(url).get('/gateway' + path);
+      expect(res1.status).to.equal(200);
+      expect(res1).to.be.json;
+      expect(res1.body).to.be.an('object');
+      expect(res1.body.status).to.equal(200);
+      expect(res1.body.payload).to.be.an('object');
+
+      if (path.startsWith('/actuators')) {
+        const res2 = await chai.request(url).put('/gateway' + path).send({
+          value: 1
+        });
+        expect(res2.status).to.equal(200);
+
+        const res3 = await chai.request(url).get('/gateway' + path);
+        expect(res3.status).to.equal(200);
+        expect(res3).to.be.json;
+        expect(res3.body).to.be.an('object');
+        expect(res3.body.status).to.equal(200);
+        expect(res3.body.payload.value).to.equal(1);
+
+        const res4 = await chai.request(url).put('/gateway' + path).send({
+          value: 0
+        });
+        expect(res4.status).to.equal(200);
+
+        const res5 = await chai.request(url).get('/gateway' + path);
+        expect(res5.status).to.equal(200);
+        expect(res5).to.be.json;
+        expect(res5.body).to.be.an('object');
+        expect(res5.body.status).to.equal(200);
+        expect(res5.body.payload.value).to.equal(0);
+      }
+    })
   });
 });
 
 // --- test helper functions ---
+const log = data => {
+  if (logEnabled) {
+    console.log(data);
+  }
+}
 // extracts resources from endpoints (copied from fog-ui's ApiBuilder)
-extractResources = endpoints => {
+const extractResources = endpoints => {
   const resources = [];
   let index = 0;
   endpoints.forEach(endpoint => {
