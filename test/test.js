3e872228bc254d0505b54217b80a9d2ec964bcae
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-12 13:22:10 +0100

    add test cases

--- test/test.js
+++ test/test.js
@@ -6,15 +6,70 @@ const expect = chai.expect;
 
 const url = 'http://localhost:8080/services';
 
-describe('mapping', () => {
-  it('should return an array', () => {
-    chai
-    .request(url)
-    .get('/mapping')
-    .then(res => {
-      expect(res.status).to.equal(200);
-      expect(res).to.be.json;
-      expect(res.body).to.be.an('object');
-    });
+console.log(`
+  NOTE: this is an integration test consisting of multiple consecutive tests.
+  The "-b" flag is set, so test execution stops after the first failing test.
+`);
+
+// --- config ---
+const log = false; // set true to print test data
+
+// --- global test variables ---
+let endpoints = [];
+let resources = [];
+
+// --- test cases ---
+describe('GET /rd/endpoints', () => {
+  it('should return a non-empty JSON array', async () => {
+    const res = await chai.request(url).get('/rd/endpoints');
+    expect(res.status).to.equal(200);
+    expect(res).to.be.json;
+    endpoints = res.body;
+    expect(endpoints).to.be.an('array');
+    expect(endpoints.length).to.be.greaterThan(0);
+    if (log) console.log('returns:');
+    if (log) console.log(endpoints);
   });
 });
+
+describe('The returned endpoints array', () => {
+  it('should consist of endpoints containing resources', async () => {
+    expect()
+    resources = extractResources(endpoints);
+    expect(resources).to.be.an('array');
+    expect(resources.length).to.be.greaterThan(0);
+    if (log) console.log('resources extracted from endpoints:');
+    if (log) console.log(resources);
+  });
+});
+
+describe('GET /mapping', () => {
+  it('should return a JSON object', async () => {
+    const res = await chai.request(url).get('/mapping');
+    expect(res.status).to.equal(200);
+    expect(res).to.be.json;
+    expect(res.body).to.be.an('object');
+  });
+});
+
+// --- test helper functions ---
+// extracts resources from endpoints (copied from fog-ui's ApiBuilder)
+extractResources = endpoints => {
+  const resources = [];
+  let index = 0;
+  endpoints.forEach(endpoint => {
+    endpoint.resources.forEach(resource => {
+      const extendedResource = {
+        ...endpoint,
+        ...resource
+      };
+      delete extendedResource.resources;
+      extendedResource.customPath = extendedResource.path;
+      extendedResource.index = index;
+      extendedResource.status = 'new';
+      index += 1;
+      resources.push(extendedResource);
+    })
+  })
+  return resources;
+}
