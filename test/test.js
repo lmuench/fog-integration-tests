2b5f11b02934b0297a60442b7f04e73acd347525
Ludwig Muench <muench.ludwig@gmail.com>
2018-11-13 17:35:46 +0100

    test DELETE

--- test/test.js
+++ test/test.js
@@ -4,12 +4,11 @@ const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const expect = chai.expect;
 
+// global constants
 const url = 'http://localhost:8080/services';
-
-console.log(`
-  NOTE: this is an integration test consisting of multiple consecutive tests.
-  The "-b" flag is set, so test execution stops after the first failing test.
-`);
+const OK = 200;
+const NO_CONTENT = 204;
+const METHOD_NOT_ALLOWED = 405;
 
 // --- config ---
 const logEnabled = true; // set true to print test data
@@ -19,11 +18,16 @@ let endpoints = [];
 let resources = [];
 let mapping = {};
 
+console.log(`
+  NOTE: this is an integration test consisting of multiple consecutive tests.
+  The "-b" flag is set, so test execution stops after the first failing test.
+`);
+
 // --- test cases ---
 describe('GET /rd/endpoints', () => {
   it('should return a non-empty JSON array', async () => {
     const res = await chai.request(url).get('/rd/endpoints');
-    expect(res.status).to.equal(200);
+    expect(res.status).to.equal(OK);
     expect(res).to.be.json;
     endpoints = res.body;
     expect(endpoints).to.be.an('array');
@@ -65,14 +69,14 @@ describe('The extracted resources', () => {
 describe('PUT /mapping', () => {
   it('should return status 204 (NO CONTENT)', async () => {
     const res = await chai.request(url).put('/mapping').send(mapping);
-    expect(res.status).to.equal(204);
+    expect(res.status).to.equal(NO_CONTENT);
   });
 });
 
 describe('GET /mapping', () => {
   it('should return the same mapping', async () => {
     const res = await chai.request(url).get('/mapping');
-    expect(res.status).to.equal(200);
+    expect(res.status).to.equal(OK);
     expect(res).to.be.json;
     expect(res.body).to.be.an('object');
     expect(res.body).to.deep.equal(mapping);
@@ -85,10 +89,10 @@ describe('GET and PUT /gateway', () => {
       log('1st GET' + path + ' request: ' + new Date().getTime());
       const res1 = await chai.request(url).get('/gateway' + path);
       log('1st GET' + path + ' response: ' + new Date().getTime());
-      expect(res1.status).to.equal(200);
+      expect(res1.status).to.equal(OK);
       expect(res1).to.be.json;
       expect(res1.body).to.be.an('object');
-      expect(res1.body.status).to.equal(200);
+      expect(res1.body.status).to.equal(OK);
       expect(res1.body.payload).to.be.an('object');
 
       if (path.startsWith('/actuators')) {
@@ -97,15 +101,15 @@ describe('GET and PUT /gateway', () => {
           value: 1
         });
         log('1st PUT' + path + ' response: ' + new Date().getTime());
-        expect(res2.status).to.equal(200);
+        expect(res2.status).to.equal(OK);
 
         log('2nd GET' + path + ' request: ' + new Date().getTime());
         const res3 = await chai.request(url).get('/gateway' + path);
         log('2nd GET' + path + ' response: ' + new Date().getTime());
-        expect(res3.status).to.equal(200);
+        expect(res3.status).to.equal(OK);
         expect(res3).to.be.json;
         expect(res3.body).to.be.an('object');
-        expect(res3.body.status).to.equal(200);
+        expect(res3.body.status).to.equal(OK);
         expect(res3.body.payload.value).to.equal(1);
 
         log('2nd PUT' + path + ' request: ' + new Date().getTime());
@@ -113,17 +117,21 @@ describe('GET and PUT /gateway', () => {
           value: 0
         });
         log('2nd PUT' + path + ' response: ' + new Date().getTime());
-        expect(res4.status).to.equal(200);
+        expect(res4.status).to.equal(OK);
 
         log('3rd GET' + path + ' request: ' + new Date().getTime());
         const res5 = await chai.request(url).get('/gateway' + path);
         log('3rd GET' + path + ' response: ' + new Date().getTime());
-        expect(res5.status).to.equal(200);
+        expect(res5.status).to.equal(OK);
         expect(res5).to.be.json;
         expect(res5.body).to.be.an('object');
-        expect(res5.body.status).to.equal(200);
+        expect(res5.body.status).to.equal(OK);
         expect(res5.body.payload.value).to.equal(0);
       }
+      log('1st DELETE' + path + ' request: ' + new Date().getTime());
+      const res = await chai.request(url).delete('/gateway' + path);
+      log('1st DELETE' + path + ' response: ' + new Date().getTime());
+      expect(res.status).to.equal(METHOD_NOT_ALLOWED);
     })
   });
 });
